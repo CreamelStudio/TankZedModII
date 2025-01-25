004cfc15062b3270e8587d294cac049d953b0c89
Glytch3r <glytch3r.arts@gmail.com>
2025-01-25 16:32:25 +0800

    fixed animation bugs

---
+++ .history/media/lua/client/TankZed_Stats_20250124212714.lua
@@ -0,0 +1,116 @@
+--[[
+----------------------------------------------------------------------------------------------------------------------------⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                       ⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣰⣦⣀⣀⠀⠠⠄⠄⠠⠀⠀⢀⣠⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⡿⢿⣦⠀⠀⠀⠀⠀⠀⠀⢀⡷⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⣩⡿⠋⠙⠉⢩⡿⠟⠀⠀⠀⠀⠀⠀⢀⣶⣾⠟⠋⠛⣿⣷⡄⠀⠀⠀⠀⠀⣴⣿⠂⠀⠀⣼⡿⠀⠀⠀⠀⠀⠀⢀⣠⡴⠶⣤⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠦⠤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠋⠀⢸⠏⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⢠⡇⠀⠀⠀⢠⣾⠋⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⢠⣿⠋⠀⠀⠀⠀⠛⠛⠁⠀⠀⠀⢀⣾⣿⠏⠀⠀⢀⡿⠃⠀⠀⠀⠀⠀⠶⠋⠁⢀⣰⠟⠀⠀⠀⠀⠀⠀⠀⣹⣿⠀⠀⠀⠀⣨⣿⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣾⡟⢀⣠⣤⣴⣶⠆⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⠀⠀⣾⠀⠀⠀⣰⣿⠃⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⠋⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⣠⣶⣯⣅⡀⠀⠀⠀⠀⠀⠀⠀⣿⡇⢀⣠⠴⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⡟⠀⠛⠉⣠⣾⠃⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⢿⣤⡴⢺⢻⠃⠀⠀⠀⠀⠀⢸⡿⠁⠀⠀⠀⠀⠀⠀⢿⠀⠀⠀⠀⢀⣤⠀⠀⠀⠀⠀⢀⣿⣇⡤⠤⢤⣾⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢻⡆⠀⠀⠀⠀⠀⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⢀⣼⠇⠀⠀⠀⠀⠀⠀⣰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢂⠇⠀⠀⠀⠀⠀⠀⣾⡅⠀⠀⠀⠀⠀⠀⠀⢸⣧⡀⠀⠀⣼⠇⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠐⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⠀⢰⡏⠘⣦⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⢧⣤⡶⠟⢻⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠏⠀⠀⠀⠀⠀⠀⢰⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣶⡾⠋⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⢰⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠞⠁⠀⠀⠀⠀⠀⠀⠁⠀⠈⠙⠂⠔⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⢀⣿⣤⠶⠒⠒⠁⠀⠀⠀⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠐⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+----------------------------------------------------------------------------------------------------------------------------
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+Please contact me if you need to hire someone to do mods or other design related tasks
+https://steamcommunity.com/id/glytch3r/myworkshopfiles/
+https://ko-fi.com/glytch3r
+Discord: Glytch3r#1337 / glytch3r
+
+
+----------------------------------------------------------------------------------------------------------------------------
+--]]
+
+--require "TankZed_Util"
+TankZedModII = TankZedModII or {}
+function TankZedModII.setStatsToAll()
+    local zombies = getCell():getZombieList()
+    if not getCell() or not zombies then return end
+    for i = 0, zombies:size() - 1 do
+        local zed = zombies:get(i)
+        if zed and TankZedModII.isTankZed(zed) then
+--[[             local spr = getSprite("media/ui/Tags/Dreadnought.png"):newInstance()
+            zed:setAttachedAnimSprite(ArrayList.new())
+            local anm = zed:getAttachedAnimSprite():add(spr) ]]
+            if zed:getModData()['TankZedII_Init'] == nil then
+				TankZedModII.setStats(zed)
+            end
+        end
+    end
+end
+
+function TankZedModII.clearMDToAll()
+    local zombies = getCell():getZombieList()
+    if not getCell() or not zombies then return end
+    for i = 0, zombies:size() - 1 do
+        local zed = zombies:get(i)
+        if zed and TankZedModII.isTankZed(zed) then
+            zed:getModData()['TankZedII_Init'] = nil
+        end
+    end
+end
+
+function TankZedModII.setStats(zed)
+    if zed and TankZedModII.isTankZed(zed) then
+        if zed:getModData()['TankZedII_Init'] == nil then
+            local sandOpt = getSandboxOptions()
+            local zCog = sandOpt:getOptionByName("ZombieLore.Cognition"):getValue()
+            local zStr = sandOpt:getOptionByName("ZombieLore.Strength"):getValue()
+            --zed:setUseless(true)
+            zed:setUseless(false)
+            zed:makeInactive(true);
+            zed:makeInactive(false);
+
+            sandOpt:set("ZombieLore.Cognition",1)
+            sandOpt:set("ZombieLore.Strength",1)
+
+
+            if zed:isFakeDead() then
+                zed:setFakeDead(false)
+            end
+            if zed:isOnFloor() then
+                zed:setCanWalk(true)
+            end
+
+            --zed:setNoTeeth(true)
+
+            --if zed:getTurnDelta() ~= 3 then zed:setTurnDelta(3) end
+
+            -----------------------            ---------------------------
+
+
+            zed:setVariable('isTankZedII', true)
+
+            zed:DoZombieStats()
+
+            TankZedModII.setWalkType(zed)
+            sandOpt:set("ZombieLore.Cognition", zCog)
+
+            sandOpt:set("ZombieLore.Strength", zStr)
+
+            zed:getModData()['TankZedII_Init'] = true
+            --getSandboxOptions():toLua()
+        end
+    end
+end
+
+function TankZedModII.getWalkTypeNum(fit, zed)
+    fit = fit or zed:getOutfitName()
+    return string.match(fit, ".*(%d+)")
+end
+
+function TankZedModII.setWalkType(zed)
+    if TankZedModII.isTankZed(zed) then
+        local fit = zed:getOutfitName()
+
+        local walkType = zed:getVariableString("zombieWalkType")
+        local num = tostring(TankZedModII.getTankZedNum(zed))
+
+        if num and walkType and num ~= walkType then
+            if num ~= walkType then
+                zed:setWalkType(num)
+            end
+        end
+
+    end
+end
